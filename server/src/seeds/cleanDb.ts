59021ebb8442818da01815d05d90b2c051504741
zachmc27 <dzdudes1213@gmail.com>
2025-05-05 10:25:16 -0500

    fixed build error

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,9 +3,9 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
+    let modelExists = await models[modelName]?.db?.db?.listCollections({
       name: collectionName
-    }).toArray()
+    })?.toArray() || [];
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
